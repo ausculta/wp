95cdecb107f426b0e29404fe47fe532e7a04bd3d
Jerome Semichon <github@ausculta.co.uk>
2019-09-17 17:43:07 +0100

    checkpoint

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -300,7 +300,7 @@ if [ -e /usr/sbin/sshd ]; then
 	eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 	/usr/sbin/sshd 
-elif
+else
 	echo >&2 "WARNING: Cannot start sshd, it does not exist."
 fi
 
