bd2df062a4a1b87c018ac11ba48fc4ddd6a40487
Jerome Semichon <github@ausculta.co.uk>
2019-11-24 20:18:35 +0000

    updated website files

--- Docker/backup.sh
+++ Docker/backup.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
 tar cpzv --exclude=website/assets -f website.tar.gz website
+
+mv shared/website.tar.gz.1 shared/website.tar.gz.2
+mv shared/website.tar.gz shared/website.tar.gz.1
+mv website.tar.gz shared/website.tar.gz
+
