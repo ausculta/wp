b3474bd0ab0a1d46ba8b4973b63f551f6108df10
Jerome Semichon <github@ausculta.co.uk>
2019-12-05 01:52:20 +0000

    Cert and google analytics

--- endeavourwp1/footer.php
+++ endeavourwp1/footer.php
@@ -13,6 +13,15 @@
     </div> <!-- /row -->
   </div><!-- /container -->
   <script src="https://kit.fontawesome.com/b3babe9ce8.js" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-106095254-1', 'auto');
+    ga('send', 'pageview');
+  </script>
   <script type='text/javascript'>
     jQuery(document).ready(function () {
       jQuery('.navbar .dropdown-item').on('click', function (e) {
