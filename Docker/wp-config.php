71e96d41d0e7e7a5cffc652c0daf8d634c196b41
Jerome Semichon <github@ausculta.co.uk>
2020-01-13 00:22:04 +0000

    Update wp-config.php

--- Docker/wp-config.php
+++ Docker/wp-config.php
@@ -77,7 +77,7 @@ $table_prefix = 'edvr1_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false);
+define( 'WP_DEBUG', true);
 define( 'WP_DEBUG_LOG', false );
 define( 'WP_DEBUG_DISPLAY', false );
 define( 'SCRIPT_DEBUG', false );
