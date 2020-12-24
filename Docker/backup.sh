a577e9a7f94ca0d4848c23872544989074ac93b2
Jerome Semichon <github@ausculta.co.uk>
2020-12-24 11:56:58 +0000

    Update backup.sh

--- Docker/backup.sh
+++ Docker/backup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-tar cpzv --exclude=/var/www/website/assets -f /var/www/website.tar.gz /var/www/website
+tar cpz --exclude=/var/www/website/assets -f /var/www/website.tar.gz /var/www/website
 
 mv /var/www/html/website.tar.gz.1 /var/www/html/website.tar.gz.2
 mv /var/www/html/website.tar.gz /var/www/html/website.tar.gz.1
